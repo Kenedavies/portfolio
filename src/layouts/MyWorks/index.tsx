9133f6af5f0f19895a24814e4e8922a6fd2d8667
shinaayomi <lateef718@gmail.com>
2023-09-23 06:34:39 +0100

    my works

---
+++ src/layouts/MyWorks/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function MyWorks() {
+  return (
+    <div>
+      <section className="px-3 md:px-6">
+        <div className="container mx-auto py-12 md:py-20">
+          <h1>My works</h1>
+        </div>
+      </section>
+    </div>
+  );
+}
