a3ff431ad1014fd5c9fdaaad105919e9eacfc7ac
shinaayomi <lateef718@gmail.com>
2023-10-14 23:11:17 +0100

    hero section

--- src/layouts/Home/index.tsx
+++ src/layouts/Home/index.tsx
@@ -6,9 +6,12 @@ import DesignProcess from "./DesignProcess";
 export default function Home() {
   return (
     <main>
-      <section className="bg-[url('/src/assets/images/Hero-lines.png')] bg-no-repeat bg-cover px-3 md:px-6 pt-28">
+      <section className="relative bg-[url('/src/assets/images/Hero-lines.png')] bg-no-repeat bg-cover px-3 md:px-6 pt-28 overflow-hidden">
+        {/* horizontal animation start */}
+        <div className="horizontal-hero-line absolute left-0 bg-app-green w-[64px] h-[1px]"></div>
+        {/* horizontal animation end */}
         <div className="container mx-auto py-12 md:py-20">
-          <div className="max-w-[750px]">
+          <div className="max-w-[750px] mx-auto flex flex-col items-center text-center">
             <div className="text-[40px] md:text-[50px] lg:text-[70px] leading-[60px] md:leading-[112px]">
               <span className="font-saira-bold">David Udemezue</span>
               <br /> <span>SaaS Product designer</span>
